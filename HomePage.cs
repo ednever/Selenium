a87894ab6c855439a535670eac97e0badd67ea15
Edgar Neverovski <edgarneverovskij@gmail.com>
2023-10-01 20:08:23 +0300

    Добавьте файлы проекта.

---
+++ HomePage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjectModel.Source.Pages
+{
+    public class HomePage
+    {
+        private IWebDriver _driver;
+
+        [FindsBy(How = How.Name, Using = "q")]
+        private IWebElement _searchtxtbox;
+
+        [FindsBy(How = How.Name, Using = "btnK")]
+        private IWebElement _searchbtn;
+
+        public HomePage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void Search(string searchText)
+        {
+            _searchtxtbox.SendKeys(searchText);
+            _searchbtn.Click();
+        }
+
+
+    }
+}
---
+++ HomeTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PageObjectModel.Source.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PageObjectModel.Tests
+{
+    public class HomeTests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void InitScript()
+        {
+            _driver = new ChromeDriver();
+        }
+
+
+        [Test]
+        public void SearchBrowserStack()
+        {
+            HomePage hp = new HomePage(_driver);
+            _driver.Navigate().GoToUrl("https://google.com");
+            hp.Search("BrowserStack");
+            Assert.True(_driver.Title.Contains("browserstack"));
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            _driver.Quit();
+        }
+    }
+}
